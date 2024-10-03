adab87f5c7daed033388a6cce92fea34ec0077a2
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-10-03 16:54:42 +0300

    add placeholder dir

---
+++ generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <project-name>"
+    exit 1
+fi
+
+PROJECT_NAME=$1
+
+if [ ! -d "placeholder" ]; then
+    echo "Error: 'placeholder' directory not found."
+    exit 1
+fi
+
+cp -r placeholder "$PROJECT_NAME"
+find "$PROJECT_NAME" -type f -exec sed -i "s/placeholder/$PROJECT_NAME/g" {} +
---
+++ placeholder/go.mod
@@ -0,0 +1,3 @@
+module github.com/iypetrov/placeholder
+
+go 1.23.1
