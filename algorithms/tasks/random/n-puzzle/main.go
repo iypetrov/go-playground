24fd3ec4c87725bf702a153446913819d252e4ff
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-11-12 01:14:11 +0200

    algorithms/tasks/random/n-puzzle: renaming vars

--- algorithms/tasks/random/n-puzzle/main.go
+++ algorithms/tasks/random/n-puzzle/main.go
@@ -4,7 +4,26 @@ import (
 	"fmt"
 )
 
-func NPuzzle(number_slots int, index_zero int, positions []int) (int, []string) {
+func FinalState(numberSlots int, indexZero int) []int {
+	index := 0
+	result := make([]int, numberSlots+1)
+	for i := 0; i <= numberSlots; i++ {
+		if i == indexZero {
+			result[i] = 0
+			continue
+		}
+		index++
+		result[i] = index
+	}
+
+	return result
+}
+
+func NPuzzle(numberSlots int, indexZero int, positions []int) (int, []string) {
+	if indexZero == -1 {
+		indexZero = numberSlots
+	}
+
 	return 0, []string{}
 }
 
--- algorithms/tasks/random/n-puzzle/main_test.go
+++ algorithms/tasks/random/n-puzzle/main_test.go
@@ -7,59 +7,59 @@ import (
 
 func TestNPuzzle(t *testing.T) {
 	tests := []struct {
-		number_slots  int
-		index_zero    int
+		numberSlots  int
+		indexZero    int
 		positions     []int
-		best_path_len int
+		bestPathLen int
 		steps         []string
 	}{
 		{
-			number_slots:  8,
-			index_zero:    0,
+			numberSlots:  8,
+			indexZero:    0,
 			positions:     []int{1, 2, 0, 3, 4, 5, 6, 7, 8},
-			best_path_len: 2,
+			bestPathLen: 2,
 			steps:         []string{"right", "right"},
 		},
 		{
-			number_slots:  8,
-			index_zero:    4,
+			numberSlots:  8,
+			indexZero:    4,
 			positions:     []int{1, 0, 3, 4, 2, 5, 6, 7, 8},
-			best_path_len: 1,
+			bestPathLen: 1,
 			steps:         []string{"up"},
 		},
 		{
-			number_slots:  8,
-			index_zero:    8,
+			numberSlots:  8,
+			indexZero:    8,
 			positions:     []int{1, 2, 3, 4, 5, 6, 0, 7, 8},
-			best_path_len: 2,
+			bestPathLen: 2,
 			steps:         []string{"left", "left"},
 		},
 		{
-			number_slots:  8,
-			index_zero:    -1,
+			numberSlots:  8,
+			indexZero:    -1,
 			positions:     []int{1, 2, 3, 4, 5, 6, 0, 7, 8},
-			best_path_len: 2,
+			bestPathLen: 2,
 			steps:         []string{"left", "left"},
 		},
 		{
-			number_slots:  15,
-			index_zero:    0,
+			numberSlots:  15,
+			indexZero:    0,
 			positions:     []int{1, 2, 3, 0, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
-			best_path_len: 3,
+			bestPathLen: 3,
 			steps:         []string{"right", "right", "right"},
 		},
 		{
-			number_slots:  15,
-			index_zero:    15,
+			numberSlots:  15,
+			indexZero:    15,
 			positions:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0, 13, 14, 15},
-			best_path_len: 3,
+			bestPathLen: 3,
 			steps:         []string{"left", "left", "left"},
 		},
 		{
-			number_slots:  15,
-			index_zero:    -1,
+			numberSlots:  15,
+			indexZero:    -1,
 			positions:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0, 13, 14, 15},
-			best_path_len: 3,
+			bestPathLen: 3,
 			steps:         []string{"left", "left", "left"},
 		},
 	}
@@ -67,9 +67,9 @@ func TestNPuzzle(t *testing.T) {
 	name := "NPuzzle"
 	algo := NPuzzle
 	for _, test := range tests {
-		len, result := algo(test.number_slots, test.index_zero, test.positions)
+		len, result := algo(test.numberSlots, test.indexZero, test.positions)
 		if !reflect.DeepEqual(result, test.steps) {
-			t.Errorf("[%s] expected %v, got %v", name, test.best_path_len, len)
+			t.Errorf("[%s] expected %v, got %v", name, test.bestPathLen, len)
 			t.Errorf("[%s] expected %v, got %v", name, test.steps, result)
 		}
 	}
