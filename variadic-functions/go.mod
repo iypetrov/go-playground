f48b0cdcbd5f30b01f7a759dc499f2d81b3b020f
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-01-30 15:16:43 +0200

    variadic-functions: init
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

---
+++ variadic-functions/go.mod
@@ -0,0 +1,3 @@
+module github.com/iypetrov/variadic-functions
+
+go 1.24.5
---
+++ variadic-functions/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func sum(nums ...int) {
+    fmt.Print(nums, " ")
+    total := 0
+
+    for _, num := range nums {
+        total += num
+    }
+    fmt.Println(total)
+}
+
+func main() {
+    sum(1, 2)
+    sum(1, 2, 3)
+
+    arr := []int{1, 2, 3, 4}
+    sum(arr...)
+
+	s := make([]int, 0, 3)
+	s = append(s, 1)
+	s = append(s, 2)
+	s = append(s, 3)
+	s = append(s, 4)
+	s = append(s, 5)
+	sum(s...)
+}
